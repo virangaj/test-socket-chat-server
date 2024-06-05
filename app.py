8f20cbb30df76cbf7ba4b09a52de3afadc89fcf3
virangaj <virangapasindu4@gmail.com>
2024-06-05 19:47:01 +0530

    change host

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ async def websocket_endpoint(websocket: WebSocket, payperviewId: str, token: str
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
